c9e60c3b59a520bf284a0c4db5cc1fb814236184
Rodrigo FERREIRA RODRIGUES <rodrigo.ferreira_rodrigues@etu.uca.fr>
2022-12-08 14:29:22 +0000

    Update EXO_3.pl

--- EXO_3.pl
+++ EXO_3.pl
@@ -19,15 +19,18 @@ ackermann(M, N, A) :- M > 0, N > 0, M2 is M - 1, N2 is N-1, ackermann(M,N2,A1), 
 % si M > 0 et N > 0 on fait 2 appel de fonction, le premier ou N prend N-1, qui retourne A1 qu on va mettre a la place de N dans la deuxieme appel de fonction ou M esr remplace par M-1
 
 % EXO 3.2.1
+chemin-oriente(X,X):- sommet(X).
 chemin-oriente(X,Y) :- arete(X,Y).
 chemin-oriente(X,Y) :- chemin-oriente(X,Z), arete(Z,Y).
 
 % EXO 3.2.2
+chemin-orienteN(X,X,0):- sommet(X).
 chemin-orienteN(X, Y, N):-chemin-orienteN(X, Z, M), M is N-1, arete(Z,Y).
 chemin-orienteN(X,Y,1) :- arete(X,Y).
 chemin-orienteN(X,Y,0) :- \+ arete(X,Y).
 
 % EXO 3.2.3
+chemin(X,X):- sommet(X).
 chemin(X,Y) :- arete(X,Y).
 chemin(X,Y) :- arete(Y,X).
 chemin(X,Y) :- chemin(X,Z), arete(Z,Y).
